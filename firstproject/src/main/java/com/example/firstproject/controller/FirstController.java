cf401d1705b102057fb5eb5a02b716d63b544953
vytndyd99 <41277173+vytndyd99@users.noreply.github.com>
2023-04-26 19:26:01 +0900

    2023-04-26
    자바 스프링부트 학습단 3장

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,4 +1,4 @@
-package com.example.firstproject.FirstController;
+package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
