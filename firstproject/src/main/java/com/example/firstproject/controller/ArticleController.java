7b76a979c42ee60ff7b0b0cd5fec2eaced6dabf9
vytndyd99 <41277173+vytndyd99@users.noreply.github.com>
2023-04-29 18:18:35 +0900

    2023-04-29
    자바 스프링부트 베타학습단 4단원 마무리

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
 
     @Autowired
@@ -21,14 +23,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
+        log.info(form.toString());
         // 1.DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+//        System.out.println(article.toString());
+        log.info(article.toString());
         // 2. 리포지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(article.toString());
-
+//        System.out.println(article.toString());
+        log.info(saved.toString());
         return "";
     }
 }
