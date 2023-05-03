3937fc2141cce0d1a6b5b07da3418ae83a3e2df2
vytndyd99 <41277173+vytndyd99@users.noreply.github.com>
2023-05-03 21:38:32 +0900

    2023-05-23
    자바 스프링부트 학습단 9단원

--- firstproject/src/main/resources/data.sql
+++ firstproject/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) values(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) values(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) values(3, '다다다다', '3333');
+INSERT INTO article(title, content) values('가가가가', '1111');
+INSERT INTO article(title, content) values('나나나나', '2222');
+INSERT INTO article(title, content) values('다다다다', '3333');
